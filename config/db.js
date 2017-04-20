fafca4447580cb9fdbc39e36bdede7192f226223
MelissaLoggins <melissaloggins@gmail.com>
2017-04-20 13:33:08 -0400

    commiting for heroku deploy

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 
 // Database configuration with Mongoose
-mongoose.connect("mongodb://localhost/MeMongoScrape");
+mongoose.connect("mongodb://heroku_5ck0jj6v:fuhteepge7jagjptfgr8nlmsbu@ds159330.mlab.com:59330/heroku_5ck0jj6v");
 var db = mongoose.connection;
 
 // Show any mongoose errors
